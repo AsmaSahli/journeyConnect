ef1a4e34b751a01cb13019589c8ad7c8894cf272
Asma Sahli <150928892+AsmaSahli@users.noreply.github.com>
2024-11-21 20:16:18 +0100

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FaSignInAlt } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSignInAlt  } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Navbar, Dropdown, Avatar } from 'flowbite-react';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { GrAddCircle } from "react-icons/gr";
+import { BsSearch } from "react-icons/bs";
+import { HiLogout } from "react-icons/hi";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { FaRoute } from "react-icons/fa";
+import logo1 from '../assets/logo1.png';
 
 const Header = () => {
-    const location = useLocation();
+
     const { currentUser } = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -29,29 +36,43 @@ const Header = () => {
     return (
         <Navbar className="border-b shadow-md dark:bg-gray-800 bg-white">
             <div className="container mx-auto flex items-center justify-between">
-                {/* Logo Section */}
+                {/* Logo1 Section */}
                 <Link to="/" className="flex items-center gap-2">
-                    <span className="text-2xl font-extrabold text-gray-900 dark:text-white">
-                        Journey<span className="font-light">Connect</span>
-                    </span>
+                <img src={logo1} alt="logo1" style={{ height: '65px', width: 'auto' }} />
                 </Link>
 
                 {/* Navigation Links */}
-                <div className="flex items-center justify-center gap-4 px-4">
-                    <Link to="/" className="text-gray-700 dark:text-gray-200 hover:text-blue-500">
-                        Home
-                    </Link>
-                    <Link to="/about" className="text-gray-700 dark:text-gray-200 hover:text-blue-500">
-                        About
-                    </Link>
-                </div>
+
 
                 {/* Theme Toggle & Authentication */}
                 <div className="flex items-center gap-4">
                     {currentUser ? (
+                        <>
+                    <div className="flex items-center justify-center gap-4 px-6">
+                    <Link
+                                to="/search-car-sharing"
+                                className="flex items-center gap-2"
+                            >
+                                
+
+                                <Button color="light"><BsSearch className="mr-3 h-4 w-4" />Search</Button>
+
+                            </Link>
+
+                            {/* Publish a Ride Button */}
+                            <Link
+                                to="/offerASeat"
+                                className="flex items-center gap-2"
+                            >
+                                <Button color="light"><GrAddCircle className="mr-3 h-4 w-4" />Publish a Ride</Button>
+
+                            </Link>
+                </div>
+
                         <Dropdown
-                            arrowIcon={false}
+                            arrowIcon={true}
                             inline
+                            size="lg"
                             label={
                                 <Avatar
                                     alt={currentUser.username}
@@ -66,12 +87,15 @@ const Header = () => {
                                     {currentUser.email}
                                 </span>
                             </Dropdown.Header>
-                            <Dropdown.Item onClick={() => navigate('/dashboard?tab=profile')}>
+                            <Dropdown.Item icon={FaRoute}>Your rides </Dropdown.Item>
+                            <Dropdown.Item icon={IoChatbubbleEllipsesOutline}>Inbox </Dropdown.Item>
+                            <Dropdown.Item icon={CgProfile} onClick={() => navigate('/dashboard?tab=profile')}>
                                 Profile
                             </Dropdown.Item>
                             <Dropdown.Divider />
-                            <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
+                            <Dropdown.Item icon={HiLogout} onClick={handleSignout}>Sign Out</Dropdown.Item>
                         </Dropdown>
+                        </>
                     ) : (
                         <Link to="/signin">
                             <Button gradientDuoTone="pinkToOrange" outline>
